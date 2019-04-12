befcdc76b25556abde5f57a54919fddadd81ff62
Karla <karla@makingdevs.com>
2019-04-12 13:19:37 -0500

    Mostrar elementos de un arreglo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ class App extends Component {
   render() {
     const style = 'active';
     const title = 'Hola Mundo';
+    const numbers = ["one", "two", "three"];
     return (
       <div className={style}>
         <h1>{title}</h1>
+        <ul>
+          {numbers.map((number) =>
+            <li>{number}</li>
+          )}
+        </ul>
       </div>
     );
   }
