7516fa211278905e15b7b9e68275f29fedfd86b8
Karla <karla@makingdevs.com>
2019-04-12 13:14:12 -0500

    Variables javascript dentro de jsx con corchetes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
+    const style = 'active';
+    const title = 'Hola Mundo';
     return (
-      <h1>Hola mundo</h1>
+      <div className={style}>
+        <h1>{title}</h1>
+      </div>
     );
   }
 }
