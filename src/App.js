cda8f436468d87041397336e65095504a19d8d8c
Karla <karla@makingdevs.com>
2019-04-15 12:44:42 -0500

    Ejemplo de manejo checkbox en un formulario

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     this.state = {
       name: "",
       title: "This title is from the state",
-      tasks: ["Task 1", "Task 2"]
+      tasks: ["Task 1", "Task 2"],
+      terms: false
     }
   }
   render() {
@@ -16,37 +17,41 @@ class App extends Component {
     const numbers = ["one", "two", "three"];
     const names = ["Alice", "Eli", "Irving"];
     return (
-      <div className={style} align="center">
+      <div className={style} >
         <h1>{title}</h1>
         <h2>{this.state.title}</h2>
         <button onClick={this.changeTitle.bind(this)}>Change the title of the state </button>
         {names.map(name =>
           <Welcome name={name}/>
           )}
-        <h1 align="left">List of elements form array</h1>  
-        <ul align="left">
+        <h1>List of elements form array</h1>  
+        <ul>
           {numbers.map((number) =>
             <li>{number}</li>
           )}
         </ul>
-        <h1 align="left">States</h1>
-        <ul align="left">
+        <h1>States</h1>
+        <ul>
           {this.state.tasks.map(task =>
             <li>{task}</li>
           )}
         </ul>
-        <button align="left" onClick={this.addTask.bind(this)}>Add new element</button>
+        <button onClick={this.addTask.bind(this)}>Add new element</button>
         <button onClick={this.updateTask.bind(this)}>Change second task</button>
         <h1>Forms</h1>
         <input type="text" value={this.state.name} onChange={this.updateName.bind(this)}/>
         <button onClick={this.sayHi.bind(this)}>Say hi!</button>
+        <h3>Checkbox Example</h3>
+        <label>
+          <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)}/>I accept the terms
+        </label>
       </div>
     );
   }
 //La inmutabilidad hace que se creen nuevos objetos en lugar de modificar los originales
   changeTitle(){
     this.setState({
-      title: "Este es el nuevo titulo del estado"
+      title: "Este es el nuevo título del estado"
     });
   }
   addTask() {
@@ -81,7 +86,17 @@ class App extends Component {
   }
 
   sayHi() {
-    alert('Hello ' + this.state.name);
+    if(this.state.terms){
+      alert('Hello ' + this.state.name);
+    } else {
+      alert("You must accept the terms");
+    }
+  }
+
+  updateTerms(event) {
+    this.setState({
+      terms: event.target.checked
+    })
   }
   
 }
